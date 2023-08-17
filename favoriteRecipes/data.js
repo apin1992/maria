d93491a1920335f00047b8e38ff2dbb6305307e6
Martin <japresaperez@gmail.com>
2023-08-17 10:53:09 -0700

    added

---
+++ favoriteRecipes/data.js
@@ -0,0 +1,15 @@
+const allRecipesData = [
+  { id: 0, name: 'Biscuits', img: 'img/image.jpeg'},
+  { id: 1, name: 'Bulgogi', img: 'img/image.jpeg'},
+  { id: 2, name: 'Calamari', img: 'img/image.jpeg'},
+  { id: 3, name: 'Ceviche', img: 'img/image.jpeg'},
+  { id: 4, name: 'Cheeseburger', img: 'img/image.jpeg'},
+  { id: 5, name: 'Churrasco', img: 'img/image.jpeg'},
+  { id: 6, name: 'Dumplings', img: 'img/image.jpeg'},
+  { id: 7, name: 'Fish & Chips', img: 'img/image.jpeg'},
+  { id: 8, name: 'Hummus', img: 'img/image.jpeg'},
+  { id: 9, name: 'Masala Dosa', img: 'img/image.jpeg'},
+  { id: 10, name: 'Pad Thai', img: 'img/image.jpeg'},
+];
+
+export default allRecipesData;
